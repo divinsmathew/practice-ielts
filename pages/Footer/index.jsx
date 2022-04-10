7970ea5c7f8c9ce931700828422e8b487f43d0c7
Divins Mathew <divinsmathew@gmail.com>
2022-04-10 22:44:19 +0530

    Added footer

---
+++ pages/Footer/index.jsx
@@ -0,0 +1,12 @@
+import styles from "./Footer.module.scss";
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <span>DM</span>
+            <span>@ 2022</span>
+        </footer>
+    );
+};
+
+export default Footer;
