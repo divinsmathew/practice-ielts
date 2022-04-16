b39c1193ce1c544f3a8acb9618afc744a352e394
Divins Mathew <divinsmathew@gmail.com>
2022-04-17 02:45:56 +0530

    Updated route structure

--- pages/listening/index.jsx
+++ pages/listening/index.jsx
@@ -1,121 +1,19 @@
-import Head from "next/head";
-
-import styles from "./Listening.module.scss";
-import Header from "components/Header";
-import Footer from "components/Footer";
-import AudioVisualizer from "components/AudioVisualizer";
-import AdsAndContainer from "components/AdsAndContainer";
-
 import { LISTENING_TESTS } from "./constants";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
 
 import { getRandomIntegerInRange } from "components/utils";
 
-const Listening = ({ testId }) => {
+const Listening = () => {
     const router = useRouter();
-    const [test, setTest] = useState();
-    const [isTestPlaying, setIsTestPlaying] = useState(false);
-    const [isQuestionsVisible, setIsQuestionsVisible] = useState(false);
-    const [isInstructionsVisible, setIsInstructionsVisible] = useState(true);
-
-    const shouldShowInstructions = () =>
-        isInstructionsVisible && !isQuestionsVisible;
-    const shouldShowQuestions = () =>
-        isQuestionsVisible && !isInstructionsVisible;
-
-    const startTest = () => {
-        setIsInstructionsVisible(false);
-        setIsQuestionsVisible(true);
-        setIsTestPlaying(true);
-    };
-
     useEffect(() => {
+        const testId = router.query.index;
         if (!testId) {
             const keys = Object.keys(LISTENING_TESTS);
-            testId = keys[getRandomIntegerInRange(0, keys.length - 1)];
+            const testId = keys[getRandomIntegerInRange(0, keys.length - 1)];
             router.push(`/listening/${testId}`, undefined, { shallow: true });
         }
-        setTest(LISTENING_TESTS[testId]);
-        console.log("[index]", testId);
     }, []);
-
-    return (
-        <>
-            <Head>
-                <title>Listening | Practice IELTS</title>
-                <meta
-                    name="Listening | Practice IELTS"
-                    content="Sharpen your listening skills and ace your IELTS exam."
-                />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <div className="container">
-                <Header fromPage="listening" />
-                <main className={styles.main}>
-                    <AdsAndContainer>
-                        <AudioVisualizer
-                            isTestPlaying={isTestPlaying}
-                            test={test}
-                        />
-                        <section
-                            className={styles["question-container"]}
-                        ></section>
-
-                        {shouldShowInstructions() && (
-                            <section
-                                className={styles["instruction-container"]}
-                            >
-                                <div className={styles["instruction-modal"]}>
-                                    <div className={styles["modal-header"]}>
-                                        Listening Test Instructions
-                                    </div>
-                                    <div className={styles["modal-body"]}>
-                                        <ul>
-                                            <li>
-                                                Listen to the audio and answer
-                                                the questions carefully.
-                                            </li>
-                                            <li>
-                                                The audio will be played only
-                                                once, pausing or re-playing
-                                                won't be allowed.
-                                            </li>
-                                            <li>
-                                                Test will auto end when the
-                                                timer runs out. You will also
-                                                have an option to end the test
-                                                early.
-                                            </li>
-                                            <li>
-                                                Click the button below to start
-                                                the test.
-                                            </li>
-                                        </ul>
-                                    </div>
-                                    <div className={styles["modal-footer"]}>
-                                        <button
-                                            className="button"
-                                            onClick={startTest}
-                                        >
-                                            Start Test
-                                        </button>
-                                    </div>
-                                </div>
-                            </section>
-                        )}
-
-                        {shouldShowQuestions() && (
-                            <section className={styles["question-container"]}>
-                                Questions
-                            </section>
-                        )}
-                    </AdsAndContainer>
-                </main>
-                <Footer />
-            </div>
-        </>
-    );
 };
 export default Listening;
