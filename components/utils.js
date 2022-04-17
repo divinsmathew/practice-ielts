ec4c552135515f58cac6c7687afd5e26da8b7ef3
Divins Mathew <divinsmathew@gmail.com>
2022-04-18 02:06:49 +0530

    Added occurance counter to utils

--- components/utils.js
+++ components/utils.js
@@ -30,4 +30,28 @@ const drawVisualizer = (canvas, normalizedData, wrapperClass) => {
     }
 };
 
-export { getRandomIntegerInRange, getTimeStampFromSeconds, drawVisualizer };
+const occurrencesCount = (string, subString, allowOverlapping) => {
+    string += "";
+    subString += "";
+    if (subString.length <= 0) return string.length + 1;
+
+    var n = 0,
+        pos = 0,
+        step = allowOverlapping ? 1 : subString.length;
+
+    while (true) {
+        pos = string.indexOf(subString, pos);
+        if (pos >= 0) {
+            ++n;
+            pos += step;
+        } else break;
+    }
+    return n;
+};
+
+export {
+    getRandomIntegerInRange,
+    getTimeStampFromSeconds,
+    drawVisualizer,
+    occurrencesCount,
+};
